3cddfafe704a1a71cab4a76656ef8f66361a947d
Andy Maleh <andy.am@gmail.com>
2020-12-23 17:53:05 -0500

    Serialize Time Ruby basic data type

--- lib/yasl.rb
+++ lib/yasl.rb
@@ -1,14 +1,49 @@
 require 'json'
+require 'date'
 
 module YASL
   class << self
     def dump(object)
+      JSON.dump(dumpable_hash(object))
+    end
+    
+    def dumpable_hash(object)
       hash = {class: {}}
       hash[:class][:name] = object.class.name
-      object.instance_variables.each do |var|
-        hash[var.to_s.sub('@', '')] = object.instance_variable_get(var)
+      if ruby_basic_data_type?(object)
+        hash[:ruby_basic_data_type_data] = ruby_basic_data_type_data(object)
+      else
+        object.instance_variables.each do |var|
+          value = object.instance_variable_get(var)
+          # TODO consider using marshal_dump and marshal_load for date objects
+          # e.g. for marshalling time, convert to datetime first: DateTime.new.marshal_load(JSON.load(JSON.dump(t.to_datetime.marshal_dump))).to_time
+          value = dumpable_hash(value) unless json_basic_data_type?(value)
+          hash[var.to_s.sub('@', '')] = value
+        end
       end
-      JSON.dump(hash)
+      hash
+    end
+    
+    def json_basic_data_type?(object)
+      object.nil? ||
+        object.is_a?(String) ||
+        object.is_a?(Numeric) ||
+        object.is_a?(Hash) ||
+        object.is_a?(Array) || # TODO check of Set and other enumerables qualify too
+        object.is_a?(TrueClass) ||
+        object.is_a?(FalseClass)
+    end
+    
+    def ruby_basic_data_type?(object)
+      json_basic_data_type?(object) ||
+        object.is_a?(Time)
+    end
+    
+    def ruby_basic_data_type_data(object)
+      # TODO Consider the new Ruby pattern matching feature for this
+      if object.is_a?(Time)
+        object.to_datetime.marshal_dump
+      end
     end
   end
 end
--- spec/lib/yasl_spec.rb
+++ spec/lib/yasl_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 RSpec.describe do
   before do
     class Car
-      attr_accessor :make, :model, :year
+      attr_accessor :make,
+                    :model,
+                    :year,
+                    :registration_time,
+                    :owner
+    end
+    class Person
+      attr_accessor :name,
+                    :dob
     end
   end
   
@@ -15,24 +23,101 @@ RSpec.describe do
     end
   end
   
-  it 'serializes instance variables' do
-    car = Car.new
-    car.make = 'Mitsubishi'
-    car.model = 'Eclipse'
-    car.year = '2002'
+  let(:car1) {
+    Car.new.tap do |car|
+      car.make = 'Mitsubishi'
+      car.model = 'Eclipse'
+      car.year = '2002'
+      car.registration_time = Time.new(2003, 10, 19, 10, 39, 37.092, '-03:00')
+#       car.registration_date = '2002'
+#       car.registration_date_time = '2002'
+    end
+  }
+  
+  let(:person1) {
+    Person.new.tap do |person|
+      person.name = 'Sean Tux'
+      person.dob = Time.new(2000, 11, 28)
+    end
+  }
+  
+  describe '#dump' do
+    it 'serializes instance variables of all Ruby basic data types' do
+      car1
+      
+      dump = YASL.dump(car1)
+      
+      expected_dump = JSON.dump(
+        class: {
+          name: car1.class.name
+        },
+        make: car1.make,
+        model: car1.model,
+        year: car1.year,
+        registration_time: {
+          class: {
+            name: 'Time'
+          },
+          ruby_basic_data_type_data: car1.registration_time.to_datetime.marshal_dump
+        }
+      )
+      expect(dump).to eq(expected_dump)
+    end
     
-    dump = YASL.dump(car)
-    expected_dump = JSON.dump(
-      class: {
-        name: 'Car'
-      },
-      make: car.make,
-      model: car.model,
-      year: car.year,
-    )
-    expect(dump).to eq(expected_dump)
+    it 'recursively (1 level deep) serializes instance variables that are not basic data types' do
+      car1
+      person1
+      car1.owner = person1
+      
+      dump = YASL.dump(car1)
+      
+      expected_dump = JSON.dump(
+        class: {
+          name: car1.class.name
+        },
+        make: car1.make,
+        model: car1.model,
+        year: car1.year,
+        registration_time: {
+          class: {
+            name: 'Time'
+          },
+          ruby_basic_data_type_data: car1.registration_time.to_datetime.marshal_dump
+        },
+        owner: {
+          class: {
+            name: 'Person'
+          },
+          name: person1.name,
+          dob: {
+            class: {
+              name: 'Time'
+            },
+            ruby_basic_data_type_data: person1.dob.to_datetime.marshal_dump
+          }
+        }
+      )
+      expect(dump).to eq(expected_dump)
+    end
+    
+    
+    xit 'serializes namespaced class' do
+    end
+    xit 'serializes date'
+    xit 'serializes time'
+    xit 'serializes date_time'
+    xit 'serializes complex'
+    xit 'serializes complex.polar'
+    xit 'serializes regex'
+    xit 'serializes symbol'
+    xit 'serializes rational'
+    xit 'serializes sets'
+    xit 'serializes enumerables'
+    xit 'serializes struct members' do
+    end
   end
   
-  xit 'serializes struct members' do
+  describe '#load' do
+    xit 'deserializes basic object (no nesting)'
   end
 end
